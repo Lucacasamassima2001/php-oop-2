3571ed41e8c417fb85891f9e4f1c37419640cac0
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-06-20 20:25:27 +0200

    some changes, added to do

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ include __DIR__ . '/data.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>E-commerce</title>
 </head>
 <body>
@@ -29,7 +30,7 @@ include __DIR__ . '/data.php';
         <h1>E-commerce</h1>
     </header>
 
-    <div class="container-big bg-danger d-flex gap-3 justify-content-center py-3 flex-wrap">
+    <div class="container-big d-flex gap-5 justify-content-center py-3 my-4 flex-wrap">
         <?php foreach($Products as $Product){
             ?>
             <div class="card" style="width: 18rem;">
@@ -51,5 +52,15 @@ include __DIR__ . '/data.php';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
+<!-- TO DO 
+
+FUNCTION TO SELECT ICON
+DISPLAY PROPERTIES
+CSS ADJUSTMENTS
+DISPLAY BY CATEGORIES 
+
+-->
 
 
